954c05add8e37e089f7ee27bde2713c853f03dcd
Vivica-R <ramdawonvivica@gmail.com>
2021-03-01 23:20:54 +0100

    created Weather component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
+import React from "react";
 import './App.css';
+import Weather from "./Weather";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <div className="container">
+      <Weather />
       <header className="App-header">
       </header>
-
       <footer>This project was coded by <a href="https://www.linkedin.com/in/vivica-ramdawon" target="_blank">Vivica Ramdawon</a> and is <a href="https://github.com/Vivica-R/react-weather-app" target="_blank">open-sourced on GitHub</a></footer>
     </div>
     </div>
   );
 }
 
-export default App;
