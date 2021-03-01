cab36f2e84a9db8a122661831474c89f2428d4b2
Vivica-R <ramdawonvivica@gmail.com>
2021-03-01 23:08:12 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
       </header>
 
       <footer>This project was coded by <a href="https://www.linkedin.com/in/vivica-ramdawon" target="_blank">Vivica Ramdawon</a> and is <a href="https://github.com/Vivica-R/react-weather-app" target="_blank">open-sourced on GitHub</a></footer>
+    </div>
     </div>
   );
 }
